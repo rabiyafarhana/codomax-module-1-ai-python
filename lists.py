3a1c62ddb91191e96df6b5957ae78b2103fb8155
rabiyafarhana <farhanarabia2006@gmail.com>
2026-08-18 19:35:55 +0530

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,17 @@
+# Codomax Internship - Module 1
+# Python Lists
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print("My fruits:", fruits)
+
+# Access an item
+print("First fruit:", fruits[0])
+
+# Add a new fruit
+fruits.append("Grapes")
+
+print("Updated fruits:", fruits)
+
+# Number of fruits
+print("Total fruits:", len(fruits))

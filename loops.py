67427208a9c14e096ab83afce5f694322aaa00ff
rabiyafarhana <farhanarabia2006@gmail.com>
2026-08-18 19:54:16 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,27 @@
+# Codomax Internship - Module 1
+# Python Loops
+
+# For loop
+print("Numbers from 1 to 5:")
+
+for number in range(1, 6):
+    print(number)
+
+# Loop through a list
+fruits = ["Apple", "Banana", "Mango"]
+
+print("\nMy fruits:")
+
+for fruit in fruits:
+    print(fruit)
+
+# While loop
+print("\nCountdown:")
+
+count = 5
+
+while count > 0:
+    print(count)
+    count -= 1
+
+print("Done!")
